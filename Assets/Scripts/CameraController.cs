c204578446dabb48aac1db5991ba23481358bcd9
alicekham <50978017+alicekham@users.noreply.github.com>
2021-07-29 17:41:13 +0800

    added more furniture

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform target;
+    public Vector3 offset;
+    public float smoothFactor;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +14,12 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
+        Vector3 targetPos = target.position + offset;
+        Vector3 smoothPos = Vector3.Lerp(transform.position, targetPos, smoothFactor*Time.fixedDeltaTime);
+        transform.position = smoothPos;
+        
+        // transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
     }
 }
